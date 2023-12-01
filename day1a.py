bfb943cf155daa1f05a84999d75e3dacbcc74bdf
Jeff Lehman <jlehman@huntington.edu>
2023-12-01 21:41:27 +0000

    starting project

--- day1a.py
+++ day1a.py
@@ -7,3 +7,16 @@
 # find the first and last digits in the string to form a two digit number
 # add all numbers from each line and output sum
 #
+
+file = open("data/day1a.txt", "r") 
+
+lines = file.readlines()
+print( lines )
+
+for line in lines:
+    print( line.strip() )
+
+
+
+
+
