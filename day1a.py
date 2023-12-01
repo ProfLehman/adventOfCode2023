c1a88b6740354326a68082a0bd3de486fe41d2cf
Jeff Lehman <jlehman@huntington.edu>
2023-12-01 16:20:21 -0500

    Create day1a.py
    Added comments for day1a

---
+++ day1a.py
@@ -0,0 +1,9 @@
+# day1a.py
+# j.l. lehman
+# December 1, 2023
+# Advent of code
+#
+# read list of strings
+# find the first and last digits in the string to form a two digit number
+# add all numbers from each line and output sum
+#
